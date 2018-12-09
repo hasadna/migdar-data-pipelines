a462df77ed246c2dda56169e9d11be612e869493
Adam Kariv <adam.kariv@gmail.com>
2018-12-09 11:03:56 +0200

    make revision an int

--- datapackage_pipelines_migdar/flows/constants.py
+++ datapackage_pipelines_migdar/flows/constants.py
@@ -7,7 +7,7 @@ SEARCH_IMPORT_FIELD_NAMES = ['Life Domains', 'Resource Type', 'Item Type', 'titl
 
 
 with open(os.path.join(os.path.dirname(__file__), 'PUBLICATIONS_REVISION.txt')) as f:
-    PUBLICATIONS_ES_REVISION = f.read().strip()
+    PUBLICATIONS_ES_REVISION = int(f.read().strip())
 PUBLICATIONS_DB_TABLE = '_elasticsearch_mirror__publications'
 PUBLICATIONS_KEY_PATTERN = 'publications/{migdar_id}'
 PUBLICATIONS_PAGE_TITLE_PATTERN = '{title}'
