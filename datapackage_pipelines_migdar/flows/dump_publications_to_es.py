50dc651f13b550885749a6029d418c36c4d77046
Adam Kariv <adam.kariv@gmail.com>
2018-12-09 10:38:42 +0200

    United publications dataset

--- datapackage_pipelines_migdar/flows/dump_publications_to_es.py
+++ datapackage_pipelines_migdar/flows/dump_publications_to_es.py
@@ -1,4 +1,5 @@
-from dataflows import Flow, load, printer, set_type, update_resource
+from datapackage import Package
+from dataflows import Flow, load, printer, set_type, update_resource, concatenate, dump_to_path, delete_fields
 from datapackage_pipelines_migdar.flows.dump_to_es import DumpToElasticSearch
 from datapackage_pipelines_migdar.flows.constants import PUBLICATIONS_ES_REVISION
 import os
@@ -16,17 +17,27 @@ def update_schema(package):
 
 def flow(*args):
     is_dpp = len(args) > 3
+    source_url = 'data/publications_for_es/datapackage.json'
+    package = Package(source_url)
+    all_fields = set(
+        field.name
+        for resource in package.resources
+        for fields in resource.schema.fields
+    )
+    all_fields = dict(
+        (field_name, [])
+        for field_name in all_fields
+    )
     return Flow(
-        load('data/publications_for_es/datapackage.json'),
-        lambda row: dict(row, json=''),
+        load(source_url),
+        lambda row: dict(row, json='{}'),
+        concatenate(all_fields, target=dict(name='publications', path='publications.csv')),
+        delete_fields(['json']),
         update_schema,
-        set_type('json', resources=None, type='object', **{'es:index': False}),
-        DumpToElasticSearch({'migdar': [{'resource-name': 'unique_records',
-                                         'doc-type': 'publications',
-                                         'revision': PUBLICATIONS_ES_REVISION}]})(),
-        DumpToElasticSearch({'migdar': [{'resource-name': 'search_app_records',
+        DumpToElasticSearch({'migdar': [{'resource-name': 'publications',
                                          'doc-type': 'publications',
                                          'revision': PUBLICATIONS_ES_REVISION}]})(),
         printer(tablefmt='plain' if is_dpp else 'html', num_rows=1, fields=['doc_id']),
+        dump_to_path('data/published_in_es'),
         update_resource(None, **{'dpp:streaming': True})
     )
