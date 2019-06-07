f863f96639e5cfb006b66d8ca660271c41323c3d
Adam Kariv <adam.kariv@gmail.com>
2019-06-07 18:15:08 +0300

    Don't limit rows

--- datapackage_pipelines_migdar/flows/publications.py
+++ datapackage_pipelines_migdar/flows/publications.py
@@ -136,8 +136,7 @@ def base_flow():
                  sheet=source['sheet'],
                  headers=source['headers'],
                  infer_strategy=load.INFER_STRINGS,
-                 cast_strategy=load.CAST_TO_STRINGS,
-                 limit_rows=3)
+                 cast_strategy=load.CAST_TO_STRINGS)
             for source in sources[0]
         ],
         load('data/zotero/zotero.csv'),
