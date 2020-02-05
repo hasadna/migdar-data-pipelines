2a682f08f51669f7ce642d9ee4639503cc86e039
Adam Kariv <adam.kariv@gmail.com>
2020-02-05 18:37:56 +0200

    Add Excel assets

--- datapackage_pipelines_migdar/flows/dataset_assets.py
+++ datapackage_pipelines_migdar/flows/dataset_assets.py
@@ -1,9 +1,48 @@
 import os
 import subprocess
 
+from openpyxl import load_workbook
+
 import dataflows as DF
 
 SCREENSHOT = os.path.join(os.path.dirname(__file__), 'node', 'screenshot.js')
+XLSX_TEMPLATE = os.path.join(os.path.dirname(__file__), 'template.xlsx')
+EXCEL_METADATA = [
+    ('B1', 'chart_title'),
+    ('B2', 'chart_abstract'),
+    ('B3', 'units'),
+    ('B4', 'source_description'),
+    ('B5', 'source_detail_description'),
+    ('B6', 'extrapulation_years'),
+]
+EXCEL_SERIES_ROW = 9
+
+
+def write_excel():
+
+    def func(row):
+        wb = load_workbook(XLSX_TEMPLATE)
+        ws = wb.active
+        for cell, field in EXCEL_METADATA:
+            ws[cell] = row.get(field, row['series'][0].get(field, ''))
+        source_range = set()
+        for x, series in enumerate(row['series']):
+            ws.cell(EXCEL_SERIES_ROW, x+2, series['gender'])
+            source_range.update(d['x'] for d in series['dataset'])
+        source_range = dict((v, i + 1) for i, v in enumerate(sorted(source_range)))
+        for value, y in source_range.items():
+            try:
+                ws.cell(EXCEL_SERIES_ROW + y, 1, int(value))
+            except:
+                ws.cell(EXCEL_SERIES_ROW + y, 1, value)
+        for x, series in enumerate(row['series']):
+            for value in series['dataset']:
+                y = source_range[value['x']]
+                ws.cell(EXCEL_SERIES_ROW + y, x+2, value['y'])
+        outpath = os.path.join('data', row['doc_id'] + '.xlsx')
+        wb.save(filename=outpath)
+
+    return func
 
 
 def do_screenshot():
@@ -20,7 +59,7 @@ def do_screenshot():
                                            lang + doc_id + suffix + '.png')
                     subprocess.call(['node', SCREENSHOT, url,
                                      outpath, '.card'])
-        return []
+            yield row
     return func
 
 
@@ -28,6 +67,7 @@ def flow(*_, path='data/datasets_in_es'):
     return DF.Flow(
         DF.load('{}/datapackage.json'.format(path)),
         do_screenshot(),
+        write_excel(),
         DF.update_resource(-1, **{'dpp:streaming': True})
     )
 
