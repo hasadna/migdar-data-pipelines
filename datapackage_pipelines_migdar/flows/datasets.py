7be6ce99fe9bc28d0ef06439982618f2f5b58663
Adam Kariv <adam.kariv@gmail.com>
2021-02-02 10:31:12 +0200

    bump datasets

--- datapackage_pipelines_migdar/flows/datasets.py
+++ datapackage_pipelines_migdar/flows/datasets.py
@@ -9,6 +9,7 @@ from datapackage_pipelines_migdar.flows.i18n import split_and_translate, fix_url
 from datapackage_pipelines_migdar.flows.constants import REVISION
 
 
+
 URLS = [
     [
         # Gender Statistics
