cb74b3f33cc7b01f3a6c4b320e5f2d23e91bc573
Adam Kariv <adam.kariv@gmail.com>
2021-11-24 16:05:25 +0200

    bumo revision

--- datapackage_pipelines_migdar/flows/constants.py
+++ datapackage_pipelines_migdar/flows/constants.py
@@ -10,6 +10,6 @@ PUBLICATIONS_KEY_PATTERN = 'publications/{migdar_id}'
 PUBLICATIONS_PAGE_TITLE_PATTERN = '{title}'
 
 today = datetime.date.today().isocalendar()
-BUMP = 3
+BUMP = 4
 REVISION = today[0] * 100 + today[1] + BUMP
 print('REVISION is {}'.format(REVISION))
